cdde22a0e9dac6c5de8dd39b4a7c30def1bd962a
Sivashankar S <sivaharicr7@gmail.com>
2025-04-11 04:31:19 +0530

    Add frontend/src/components/layout/Layout.jsx 1744326077040

---
+++ frontend/src/components/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <main className="py-10">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+}
